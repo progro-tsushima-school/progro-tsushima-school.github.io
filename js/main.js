4591a38d3c481452928f2eb80e7ab035e626e278
Taiga10969 <taiga10969.uta@gmail.com>
2025-12-14 12:12:24 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // js/main.js
-import { members, roleLabels } from "./config.js?v=2025-12-13";
+import { members, roleLabels } from "./config.js?v=2025-12-14";
 
 const container = document.getElementById("members");
 
